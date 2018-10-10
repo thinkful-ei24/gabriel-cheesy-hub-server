9ff35398f2cbb173263aeb479bc0646b55f71798
Gabriel Morris <gabemorris@outlook.com>
2018-10-10 13:48:26 -0700

    initial server configuration

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
 
 const app = express();
 
@@ -21,6 +20,69 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+// app.use('/api/cheeses', (req, res, next) => {
+//   const cheeses = [
+//     'Bath Blue',
+//     'Barkham Blue',
+//     'Buxton Blue',
+//     'Cheshire Blue',
+//     'Devon Blue',
+//     'Dorset Blue Vinney',
+//     'Dovedale',
+//     'Exmoor Blue',
+//     'Harbourne Blue',
+//     'Lanark Blue',
+//     'Lymeswold',
+//     'Oxford Blue',
+//     'Shropshire Blue',
+//     'Stichelton',
+//     'Stilton',
+//     'Blue Wensleydale',
+//     'Yorkshire Blue'
+//   ];
+
+//   res.json(cheeses);
+// });
+
+app.use('/api/diary', (req, res, next) => {
+  const a = {
+    error: null,
+    diaryFilms: [
+      {
+        diaryID: '12345',
+        imdbID: 'tt3896198',
+        title: 'Guardians of the Galaxy Vol. 2',
+        plot:
+          'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+        actors: ['Jessica Chastain', 'Joaquin Phoenix']
+      },
+      {
+        diaryID: '22345',
+        imdbID: 'tt3896198',
+        title: 'Guardians of the Galaxy Vol. 2',
+        plot:
+          'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+        actors: ['Jessica Chastain', 'Joaquin Phoenix']
+      },
+      {
+        diaryID: '32345',
+        imdbID: 'tt3896198',
+        title: 'Guardians of the Galaxy Vol. 2',
+        plot:
+          'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+        actors: ['Jessica Chastain', 'Joaquin Phoenix']
+      }
+    ],
+    searchFilms: [
+      {
+        imdbID: 'tt3896198',
+        title: 'Guardians of the Galaxy Vol. 2'
+      }
+    ]
+  };
+  res.json(a);
+});
 
 function runServer(port = PORT) {
   const server = app
